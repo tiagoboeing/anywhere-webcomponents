256b32e4369857b0053dd37cf5b038619987bd11
Tiago Boeing <contato@tiagoboeing.com>
2020-08-26 22:51:25 -0300

    feature: #5 Add outline mode to buttons

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,6 +4,13 @@ import { sass } from '@stencil/sass';
 export const config: Config = {
   namespace: 'anywhere-webcomponents',
   taskQueue: 'async',
+  testing: {
+    /**
+     * Gitlab CI doesn't allow sandbox, therefor this parameters must be passed to your Headless Chrome
+     * before it can run your tests
+     */
+    browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
   outputTargets: [
     {
       type: 'dist',
