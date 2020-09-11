85dcc6dc46a37368d52a9cbab686b00d3c5b5d5e
Tiago Boeing <contato@tiagoboeing.com>
2020-09-11 07:29:55 -0300

    CloudFront dist ID

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,4 +10,4 @@ aws s3 sync loader ${BUCKET}${GITHUB_REF##*/}/loader/
 echo "Copy to S3 concluded"
 echo "Prepare to invalidade cache on CloudFront"
 
-aws cloudfront create-invalidation --distribution-id ${{ secrets.AWS_CLOUDFRONT_DIST_ID }} --paths "/anywhere-webcomponents/${GITHUB_REF##*/}/*"
+aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DIST_ID} --paths "/anywhere-webcomponents/${GITHUB_REF##*/}/*"
