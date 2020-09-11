631755b5cf7b8ca35b062285d7edb97136881575
Tiago Boeing <contato@tiagoboeing.com>
2020-09-11 07:15:22 -0300

    Add loader to subfolder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 BUCKET=s3://cdn.tiagoboeing.com/anywhere-webcomponents/
 
 aws s3 sync dist ${BUCKET}${GITHUB_REF##*/}
-aws s3 sync loader ${BUCKET}${GITHUB_REF##*/}
-aws cloudfront create-invalidation --distribution-id ${{ secrets.AWS_CLOUDFRONT_DIST_ID }} --paths "/anywhere-webcomponents/${GITHUB_REF##*/}/*"
+aws s3 sync loader ${BUCKET}${GITHUB_REF##*/}/loader/
+aws cloudfront create-invalidation --distribution-id ${secrets.AWS_CLOUDFRONT_DIST_ID} --paths "/anywhere-webcomponents/${GITHUB_REF##*/}/*"
