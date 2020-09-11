03b374864ab9c74e9e29ca92457edd2ed61910cd
Tiago Boeing <contato@tiagoboeing.com>
2020-09-11 07:25:09 -0300

    deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+echo "Deploy to CDN"
+
 BUCKET=s3://cdn.tiagoboeing.com/anywhere-webcomponents/
 
 aws s3 sync dist ${BUCKET}${GITHUB_REF##*/}
 aws s3 sync loader ${BUCKET}${GITHUB_REF##*/}/loader/
+
+echo "Copy to S3 concluded"
+echo "Prepare to invalidade cache on CloudFront"
+
 aws cloudfront create-invalidation --distribution-id ${{ secrets.AWS_CLOUDFRONT_DIST_ID }} --paths "/anywhere-webcomponents/${GITHUB_REF##*/}/*"
